d0b25b66562d26183fd8b2b84cdcb4f175ee8f8a
I putu Rama anadya <iputuramaanadya@IPs-Mac-mini.local>
2024-05-19 18:26:07 +0800

    add setting sheet

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -13,6 +13,7 @@ struct Cardview: View {
     
     @State private var imageNumber: Int = 1
     @State private var randomNumber: Int = 1
+    @State private var isShowingSheet: Bool = false
     
     // MARK: - FUNCTIONS
     
@@ -52,10 +53,19 @@ struct Cardview: View {
                         Button(
                             action: {
                                 // ACTION: Show a Sheet
+                                isShowingSheet.toggle()
                             },
                             label: {
                             CustomButtonView()
                         })
+                        .sheet(
+                            isPresented: $isShowingSheet,
+                            content: {
+                                SettingsView()
+                                    .presentationDragIndicator(.visible)
+                                    .presentationDetents([.medium, .large])
+                            }
+                        )
                     }
                     Text("Fund and enjoyable outdoor activity for friends and families")
                         .multilineTextAlignment(.leading)
