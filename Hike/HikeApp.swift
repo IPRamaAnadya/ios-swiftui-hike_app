6e6ec2f75dd05bbe1c3149db50cfa4484ff78392
IPRamaAnadya <gusrama18@gmail.com>
2024-05-19 10:32:24 +0800

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by I putu Rama anadya on 19/05/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
